2bc8a7c4cd2288c32a348a9f8f3553949b714c62
Prosta4ok_ua <danyavdele@gmail.com>
2023-02-09 03:18:57 +0200

    yes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const mindustry = require('../logic/test');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,20 @@ router.get('/', function(req, res, next) {
 router.get('/error', function(req, res, next) {
   res.render('error', { message: 'message' });
 });
-router.get("/version")
+router.get("/version", (req, res, next) => {
+  let version =  mindustry.sendToConsole("version");
+  console.log(mindustry.stack.length)
+
+
+  let strs = mindustry.stack.toString().split(",")
+  console.log("111",strs)
+  console.log(strs[strs.length - 1])
+  //
+
+  if (mindustry.isConnected === true)
+    res.render('version', {version: version , hehe: mindustry.stack, var1: mindustry.stack.pop()})
+  else
+    res.render('error', {message: "Mindustry server is not running.", message2: version})
+})
 
 module.exports = router;
